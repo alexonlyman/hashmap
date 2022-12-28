ea0916ecf545cf9fdeed0573606271b72b31df40
Алекс <chu@mail.ru>
2022-12-28 12:30:53 +0300

    задание1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> telephoneMap = new HashMap<>();
+        telephoneMap.put("Борисов Ярослав", "77174909644");
+        telephoneMap.put("Волков Тимофей", "77274909644");
+        telephoneMap.put("Голиков Ярослав", "77374909644");
+        telephoneMap.put("Горелов Владимир", "77434909644");
+        telephoneMap.put("Григорьев Марк", "77054509644");
+        telephoneMap.put("Давыдов Евгений", "77066909644");
+        telephoneMap.put("Ефремов Владими", "77776909644");
+        telephoneMap.put("Зимин Антон", "77874909744");
+        telephoneMap.put("Иванов Арсений", "77974609644");
+        telephoneMap.put("Котов Артём", "77174909844");
+        System.out.println(telephoneMap);
+
+
+
+    }
+}
