11889f6bf0c1efb44b0d2f4d05630c3c97e59abe
Алекс <chu@mail.ru>
2023-01-01 16:20:43 +0300

    исправил

--- src/NewMap.java
+++ src/NewMap.java
@@ -5,8 +5,9 @@ public class NewMap {
     private static final Map<String, Integer> intMap = new HashMap<>();
 
     private static void addNumber(String s, Integer i) {
-        if (intMap.containsKey(s) && intMap.containsKey(i)) {
+        if (intMap.containsKey(s) && intMap.containsValue(i)) {
             throw new RuntimeException(intMap + "значения совпадают");
+
         }
         intMap.put(s, i);
     }
